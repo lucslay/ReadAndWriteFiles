cc981b3821d28fec5e7105f9951577d36abdedc9
Lucas <lucas_layton1@baylor.edu>
2022-09-08 18:47:09 -0500

    files

--- employee_bonus.py
+++ employee_bonus.py
@@ -1,6 +1,14 @@
 import csv
 
 infile = open ("EmployeePay.csv" , "r")
-csvreader = csv.reader(infile, delimiter=",")
-for row in infile:
-    print(row)
+csv_reader = csv.DictReader(infile, delimiter = ',')
+
+idCount = 1
+for row in csv_reader:
+    newBonus = float(row["Bonus"]) + 1.0
+    salaryWithBonus = float(row["Salary"]) * newBonus
+    print('Employee ID:', idCount,', Name:', row["EmpFName"] , row["EmpLName"], ', Employee Salary with the Bonus:', salaryWithBonus)
+    idCount += 1
+    ask = input("Would you like to view the next employee? Press y to continue. Press n to stop.")
+    if ask == "n":
+        break
